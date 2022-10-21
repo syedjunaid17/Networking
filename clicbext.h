d90519cfd09e0869a57908e89500307690061636
syedjunaid17 <106436616+syedjunaid17@users.noreply.github.com>
2022-10-21 20:25:12 +0530

    Add files via upload

---
+++ clicbext.h
@@ -0,0 +1,14 @@
+/*
+Filename:  clicbext.h
+Description:  Header file for cli Default Callback Extension functions
+ */
+
+#ifndef __CLICBEXTN__
+#define __CLICBEXTN__
+
+void
+terminate_signal_handler();
+
+void
+collect_supportsave_data();
+#endif /* __CLICBEXTN__ */
